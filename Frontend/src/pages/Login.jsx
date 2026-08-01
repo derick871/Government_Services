0fbe783b0ef985256f232f73c8952398a560f74d
derick871 <wanyamaderrick4@gmail.com>
2026-08-01 21:59:03 +0300

    feat: create Login page structure and form state

---
+++ Frontend/src/pages/Login.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export default function Login() {
+    const [email, setEail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const loginUser = async (e) => {
+        e.preventDefault();
+        alert("login successful");
+
+}
+
+return (
+    <div className="bg:slate-750, m-10">
+        <form
+        onSubmit={login}>
+            <h2>
+                Citizen Login
+            </h2>
+            <input type="email" 
+            placeholder="Email"
+          className="w-full border p-2 mb-4"
+          onChange={(e)=>setEmail(e.target.value)}
+        />
+        <input type="password"
+        placeholder="Password"
+        className="w-full border p-2 mb-4"
+        onChange={(e)=>setPassword(e.target.value)}
+        />
+        <button type="submit" className="bg:amber-500, text-white, p-2, rounded-md">
+            Login
+        </button>
+        </form>
+    </div>
+
+)
+}
+
