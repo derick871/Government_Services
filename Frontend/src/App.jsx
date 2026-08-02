50c00ca52ff7ca6aae793ff01c4d436e614a3465
derick871 <wanyamaderrick4@gmail.com>
2026-08-02 10:03:12 +0300

    install tailwindcss and add it to the vite.config file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import adminconsole from "./pages/adminconsole";
+import TrackService from "./pages/TrackService";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/adminconsole" element={<adminconsole />} />
+        <Route path="/trackservice" element={<TrackService />} />
       </Routes>
     </BrowserRouter>
   );
